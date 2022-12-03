287ff55683d359c0b815f712a56c3020fa6b09ce
Pawel Morawski <pawlo.morawski@gmail.com>
2022-12-03 19:29:30 +0100

    add solution for part2 od day02

--- AoC_2022/Day02/Day02.cpp
+++ AoC_2022/Day02/Day02.cpp
@@ -1,9 +1,10 @@
+#include <functional>
 #include <iostream>
 
 #include "Utils.h"
 
 
-int roundResult(const char opponent, const char player)
+int roundResultForPart1(const char opponent, const char player)
 {
 	// points:
 	// for player character: 1 for Rock, 2 for Paper, and 3 for Scissors
@@ -54,7 +55,60 @@ int roundResult(const char opponent, const char player)
 	}
 }
 
-int processFileData(const std::string& data)
+int roundResultForPart2(const char opponent, const char result)
+{
+	// points:
+	// for player character: 1 for Rock, 2 for Paper, and 3 for Scissors
+	// for result: 0 if you lost, 3 if the round was a draw, and 6 if you won
+	//
+	// X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win
+	switch (opponent)
+	{
+	case 'A': // rock
+		switch (result)
+		{
+		case 'X': // lose - scissors
+			return 3;
+		case 'Y': // draw - rock
+			return 4;
+		case 'Z': // win - paper
+			return 8;
+		default:
+			throw std::runtime_error("wrong player character");
+		}
+
+	case 'B': // paper
+		switch (result)
+		{
+		case 'X': // lose - rock
+			return 1;
+		case 'Y': // draw - paper
+			return 5;
+		case 'Z': // win - scissors
+			return 9;
+		default:
+			throw std::runtime_error("wrong player character");
+		}
+
+	case 'C': // scissors
+		switch (result)
+		{
+		case 'X': // lose - paper
+			return 2;
+		case 'Y': // draw - scissors
+			return 6;
+		case 'Z': // win - rock
+			return 7;
+		default:
+			throw std::runtime_error("wrong player character");
+		}
+
+	default:
+		throw std::runtime_error("wrong opponent character");
+	}
+}
+
+int processFileData(const std::string& data, const std::function<int(char, char)>& calculateFunction)
 {
 	int result{};
 	try
@@ -73,7 +127,7 @@ int processFileData(const std::string& data)
 				else
 				{
 					const char player = ch;
-					result += roundResult(opponent, player);
+					result += calculateFunction(opponent, player);
 					isOpponentLoaded = false;
 				}
 			}
@@ -91,7 +145,10 @@ int main()
 {
 	// part 1
 	const auto data = Utils::read_file("day02_input.txt");
-	const auto result = processFileData(data);
+	auto result = processFileData(data, roundResultForPart1);
+	std::cout << "Result is: " << result << "\n";
 
+	// part 2
+	result = processFileData(data, roundResultForPart2);
 	std::cout << "Result is: " << result << "\n";
 }
