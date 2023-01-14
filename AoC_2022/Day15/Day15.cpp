6e07caa41d4265bb37c07ace50621a65599b5e54
Paweł Morawski <pawlo.morawski@gmail.com>
2023-01-14 15:09:38 +0100

    day15 init commit

---
+++ AoC_2022/Day15/Day15.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Utils.h"
+
+int main()
+{
+	const auto data = Utils::read_file("day15_simple.txt");
+	const auto lines = Utils::split_string(data, '\n');
+
+	// part 1
+
+	// part 2
+}
